fbe1c7d99d9e44ac4296a8aa44076f35f88827f2
Tamim Jabr <tj222kg@student.lnu.se>
2021-07-20 21:11:30 +0200

    keep only needed code

--- main.py
+++ main.py
@@ -3,70 +3,6 @@ import time
 import pycom
 from machine import Pin
 from machine import Timer
-from machine import ADC
-
-# LightSensorPin = 'P20'  # sensor connected to P20. Valid pins are P13 to P20.
-
-# lightPin = Pin(LightSensorPin, mode=Pin.IN)  # set up pin mode to input
-# #
-# # create an ADC object bits=10 means range 0-1024 the lower value the less light detected
-# adc = ADC(bits=10)
-# # create an analog pin on P16;  attn=ADC.ATTN_11DB measures voltage from 0.1 to 3.3v
-# apin = adc.channel(attn=ADC.ATTN_11DB, pin=LightSensorPin)
-
-# while True:
-#     val = apin()  # read an analog value
-#     print("Value", val)
-#     time.sleep(1)  # wait 1 sec
-
-
-# temperature
-# adc = machine.ADC()
-# apin = adc.channel(pin='P16')
-
-
-# while True:
-#   millivolts = apin.voltage()
-#   celsius = (millivolts - 500.0) / 10.0
-
-#   print(celsius)
-
-#   time.sleep(1)
-
-
-# Motion
-# motionDetected = 1
-# noMotionDetected = 0
-# hold_time_sec = 0.1
-# pir = Pin('P4', mode=Pin.IN)
-
-# chrono = Timer.Chrono()
-# chrono.start()
-
-# print("Starting Detection")
-# while True:
-#     if pir() == motionDetected:
-#         print(chrono.read(), "Motion Detected!")
-#     # print(pir())
-
-#     if pir() == noMotionDetected:
-#         pass
-
-#     time.sleep(hold_time_sec)
-
-# # Button
-
-# p_in = Pin('P10', mode = Pin.IN)  # SENSOR BUTTON FORM ELEKTROKIT
-# # p_in = Pin('P10', mode=Pin.IN, pull=Pin.PULL_UP)  # A REGULAR BUTTON PULLED UP
-
-# while(1):
-#     val = p_in() # get value, 0 or 1
-#     if val == 1:
-#         print("NOT PRESSED !")
-#     else:
-#         print("PRESSED !")
-
-#     time.sleep(1)
 
 
 powerOn = False
@@ -120,34 +56,3 @@ while(1):
                     pycom.rgbled(0x000000)
                     break
     time.sleep(1)
-
-    # motionDetected = 1
-    # noMotionDetected = 0
-    # hold_time_sec = 0.1
-    # pir = Pin('P4', mode=Pin.IN)
-
-    # chrono = Timer.Chrono()
-    # chrono.start()
-
-    # print("Starting Detection")
-    # while True:
-    #     if pir() == motionDetected:
-    #         print(chrono.read(), "Motion Detected!")
-    #     # print(pir())
-
-    #     if pir() == noMotionDetected:
-    #         pass
-
-    #     time.sleep(hold_time_sec)
-
-
-# while True:
-#     #The value can be a sensor reading being done here
-#     value = 5
-
-#     #Sending to pybytes in channel 1
-#     pybytes.send_signal(1, value)
-#     print("sending: {}".format(value))
-
-#     #Send every 5 seconds
-#     time.sleep(5)
